ae755c3f1cb8c9a9d26e1c0c4edd068363edf6e5
Midnight Sun <cerrina77@yahoo.com>
2013-04-03 12:36:44 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Midnight::Application.config.secret_token = 'e46cf5905bf4d250b141b2b90785eb8b5970152195a4375b6a6b920acab9bb9953644d55cfa847883c41ceb84dd89aca94d060e53db623646a5587d59e5822ba'
